f3e3fa57d4248ea9e9d11be54f372aed91742951
alexnguyen104 <vstar.alex104@gmail.com>
2025-09-30 23:30:37 +1000

    add query url to homepage button and check for empty input

--- public/main.js
+++ public/main.js
@@ -1,24 +1,42 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// import './style.css'
+// import javascriptLogo from './javascript.svg'
+// import viteLogo from '/vite.svg'
+// import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//     <a href="https://vite.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+//     </a>
+//     <h1>Hello Vite!</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite logo to learn more
+//     </p>
+//   </div>
+// `
 
-setupCounter(document.querySelector('#counter'))
+// setupCounter(document.querySelector('#counter'))
+
+let btn = document.getElementsByClassName("searchButton")[0]
+let input = document.getElementsByClassName("searchInput")[0]
+
+input.onfocus = function(){
+  input.value = ''
+  input.style.color = 'var(--primary-color)'
+}
+
+btn.onclick = function(){
+  if(input.value == ''){
+    // alert("Can't find the course!")
+    input.value = "Can't find the course!";
+    input.style.color = "#ff7878ff";
+    return;
+  }
+  window.location.href = '/calendar?courseid=' + input.value.toUpperCase();
+}
